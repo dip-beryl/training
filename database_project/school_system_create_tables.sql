0c18bf4a33b2b8c69e3d1336a6b4bb41b2b4df2d
dip-beryl <dipanshu.ranga@berylsystems.com>
2022-07-25 18:54:21 +0530

    add database_project (school_system)

---
+++ database_project/school_system_create_tables.sql
@@ -0,0 +1,119 @@
+-- THIS IS THE SQL FILE TO GENERTAE THE STRUCTURE OF "school_sysytem" DATABASE
+
+-- File Name       : school_system_create_tables.sql
+-- Author          : Dipanshu Ranga (@dip-beryl)
+-- Organization    : Beryl Systems Pvt. Ltd.
+
+
+-- CREATE THE DATABASE
+DROP DATABASE IF EXISTS school_system;
+CREATE DATABASE school_system;
+
+-- CONNECT TO THE DATABASE
+\connect school_system
+
+-- CREATE TABLE STUDENT
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
+    student_id  SERIAL PRIMARY KEY NOT NULL UNIQUE,
+    first_name  VARCHAR(20) NOT NULL,
+    last_name   VARCHAR(20) NOT NULL,
+    gender      char(1) NOT NULL CHECK(gender = 'M' OR gender = 'F'),
+    dob         date NOT NULL,
+    contact     VARCHAR(15),
+    date_of_admission DATE NOT NULL,
+    family_id   INTEGER NOT NULL
+);
+
+-- CREATE FAMILY DATABASE
+DROP TABLE IF EXISTS family;
+CREATE TABLE family(
+    family_id       SERIAL PRIMARY KEY NOT NULL UNIQUE,
+    f_first_name    VARCHAR(20)  NOT NULL,
+    f_last_name     VARCHAR(20),
+    m_first_name    VARCHAR(20),
+    m_last_name     VARCHAR(20),
+    contact         VARCHAR(15) NOT NULL,
+    contact_2       VARCHAR(15),
+    email           VARCHAR(254),
+    address         TEXT NOT NULL
+);
+
+-- SETTING THE FOREIGN KEY OF student.family_id TO family.family_id
+ALTER TABLE ONLY student ADD CONSTRAINT student_family_id_fkey FOREIGN KEY (family_id) REFERENCES family(family_id);
+
+-- CREATE DEPARTMENT TABLE
+DROP TABLE IF EXISTS department;
+CREATE TABLE department(
+    dept_id     INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    dept_name   VARCHAR(20)     NOT NULL
+);
+
+-- CREATE EMPLOYEE TABLE
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee(
+    employee_id SERIAL  PRIMARY KEY NOT NULL UNIQUE,
+    first_name  VARCHAR(20) NOT NULL,
+    last_name   VARCHAR(20) NOT NULL,
+    gender      char(1) NOT NULL CHECK(gender = 'M' OR gender = 'F'),
+    dob         date NOT NULL,
+    contact     VARCHAR(15) NOT NULL,
+    email       VARCHAR(254) NOT NULL,
+    address     TEXT NOT NULL,
+    date_of_join    DATE    NOT NULL,
+    dept_id     INTEGER NOT NULL REFERENCES department(dept_id)
+    account_no  TEXT,
+    salary  REAL
+);
+
+-- CREATE A SUBJECT TABLE
+DROP TABLE IF EXISTS subject;
+CREATE TABLE subject(
+    subject_id  INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    subject_name    VARCHAR(50) NOT NULL
+);
+
+-- CREATE A SEPERATE TEACHER TABLE FROM employee TABLE
+DROP TABLE IF EXISTS teacher;
+CREATE TABLE teacher(
+    teacher_id      INTEGER  NOT NULL REFERENCES employee(employee_id) UNIQUE,
+    subject_id    INTEGER             REFERENCES subject(subject_id)
+);
+
+-- CREATE GRADE TABLE
+DROP TABLE IF EXISTS grade;
+CREATE TABLE grade(
+    grade_id   INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    grade_name VARCHAR(20) NOT NULL UNIQUE
+);
+
+-- CRAETE CLASSROOM TABLE
+DROP TABLE IF EXISTS classroom;
+CREATE TABLE classroom(
+    classroom_id    INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    grade_id        INTEGER     REFERENCES grade(grade_id),
+    teacher_id      INTEGER     REFERENCES teacher(teacher_id)
+);
+
+-- CREATE A CONNECTION TABLE classrom_student
+DROP TABLE IF EXISTS classroom_student;
+CREATE TABLE classroom_student(
+    classroom_id    INTEGER    REFERENCES classroom(classroom_id),
+    student_id      INTEGER    REFERENCES student(student_id)
+);
+
+-- CREATE ATTENDANCE TABLE FOR STUDENTS
+DROP TABLE IF EXISTS attendance_student;
+CREATE TABLE attendance_student(
+    date    DATE    NOT NULL,
+    student_id  INT REFERENCES student(student_id),
+    status  BOOLEAN
+);
+
+-- CREATE ATTENDANCE TABLE FOR EMPLOYEES
+DROP TABLE IF EXISTS attendance_employee;
+CREATE TABLE attendance_employee(
+    date    DATE    NOT NULL,
+    employee_id  INT REFERENCES employee(employee_id),
+    status  BOOLEAN
+);
---
+++ database_project/school_system_insert_data.sql
@@ -0,0 +1,179 @@
+-- SQL FILE TO INSERT DATA IN DATABASE school_system
+
+-- File Name       : school_system_insert_data.sql
+-- Author          : Dipanshu Ranga (@dip-beryl)
+-- Organization    : Beryl Systems Pvt. Ltd.
+
+
+
+-- INSERT VALUES IN family
+INSERT INTO family
+VALUES
+    (DEFAULT, 'CHAND', 'KISHORE', 'USHA', 'RANI', '9899717763', NULL, 'chandki@gmail.com', 'SGM NAGAR'),
+    (DEFAULT, 'RAVI', 'VIRMANI', 'ASHA', 'VIRMNAI', '9899755556', NULL,'ravi99@gmail.com', 'NIT 2'),
+    (DEFAULT, 'VIJAY', 'SHARMA', 'ANJALI', 'SHARMA', '8273749187', NULL, 'vijays@gmail.com', 'Rahul Colony'),
+    (DEFAULT, 'RAM', 'LAL', NULL, NULL, '9898989836', NULL, NULL, 'Aggarwal Colony'),
+    (DEFAULT, 'KATRINA', NULL, NULL, NULL, '9162735282', NULL, 'katrina@gmail.com', 'SECTOR 11'),
+    (DEFAULT, 'PARDEEP', 'CHHABRA', 'ANITA', 'CHHABRA', '7838298374', '8783726178','anita@yahoo.com', 'DELHI'),
+    (DEFAULT, 'SURAJ', 'ARORA', 'SUMAN', 'ARORA', '8782738289', '8672671698', 'suraj@gmail.com', 'Faridabad'),
+    (DEFAULT, 'SYAM', 'GUPTA', 'PRIYA', 'GUPTA', '7867356782', NULL, NULL, 'NIT 3'),
+    (DEFAULT, 'KISHAN', 'SINGH', 'RAVI', 'DEVI', '9898989897', NULL, NULL, 'Palwal'),
+    (DEFAULT, 'AMAN', 'GUPTA', 'SUHASI', 'GUPTA', '977376627', '9896367267', 'suhasi@gmail.com', 'SECTOR 15, FARIDABAD')
+;
+
+
+-- INSERTING STUDNENT VALUESINSERT INTO student 
+INSERT INTO student
+VALUES
+    (DEFAULT, 'RAHUL', 'GANDHI', 'M', '1999-02-12', '9898982347', '2001-04-03', 2),
+    (DEFAULT, 'SADHVI','BHATIA','F','1997-05-01','9293745182','2010-04-04',3),
+    (DEFAULT, 'DEEPAK','CHAURASIA','M','1998-11-11','9800000023','2015-05-10',10),
+    (DEFAULT, 'AJAY','AGRAWAL','M','2000-10-13',NULL,'2016-05-01',4),
+    (DEFAULT, 'POOJA', 'ARYA', 'F', '1997-06-27', '9829386756', '2010-04-20', 4),
+    (DEFAULT, 'DIPNASHU', 'RANGA', 'M', '1997-12-03', '7838985501', '2009-03-30', 1),
+    (DEFAULT, 'DHRUV', 'KUMAR', 'M', '2005-01-10', NULL, '2010-04-03',5),
+    (DEFAULT, 'ANJALI', 'RANA', 'F', '1996-10-12', NULL, '2011-05-01',7),
+    (DEFAULT, 'JOHN', 'WICK', 'M', '1999-12-12', NULL, '2014-03-06', 9),
+    (DEFAULT, 'ADAM', 'PAUL', 'M', '2004-01-29', NULL, '2012-04-01',8),
+    (DEFAULT, 'JYOTI', 'SINGH', 'F', '2003-05-25', '8787873567', '2012-03-27', 6)
+;
+
+
+--INSERNG IN TO subject TABLE
+INSERT INTO subject
+VALUES
+    (001, 'MATHEMATICS'),
+    (002, 'SCIENCE'),
+    (003, 'SOCIAL SCIENCE'),
+    (004, 'HINDI'),
+    (005, 'ENGLISH'),
+    (006, 'ARTS'),
+    (007, 'MUSIC'),
+    (008, 'PHYSICAL EDUCATION'),
+    (009, 'HISTORY'),
+    (010, 'GEOGRAPHY'),
+    (011, 'POLITICAL SCIENCE'),
+    (012, 'PHYSICS'),
+    (013, 'CHEMISTERY'),
+    (014, 'BI0LOGY'),
+    (015, 'SANSKRIT')
+;
+
+
+-- INSERTING VALUES INTO grade TABLE
+INSERT INTO grade
+VALUES
+    (100, 'PRE NURSURY'),
+    (101, 'NURSURY'),
+    (102, 'LKG'),
+    (103, 'UKG'),
+    (001, '1st'),
+    (002, '2nd'),
+    (003, '3rd'),
+    (004, '4th'),
+    (005, '5th'),
+    (006, '6th'),
+    (007, '7th'),
+    (008, '8th'),
+    (009, '9th'),
+    (010, '10th'),
+    (011, '11th'),
+    (012, '12th')
+;
+
+
+-- INSERTING VALUES INTO department TABLE
+INSERT INTO department
+VALUES
+    (001, 'FINANCE'),
+    (002, 'EDUCATION'),
+    (003, 'ACADEMIC'),
+    (004, 'HOUSEKEEPING'),
+    (005, 'BOARD')
+;
+
+-- INSERING INTO employee TABLE
+INSERT INTO employee
+VALUES
+    (DEFAULT, 'PRIYANKA', 'SOLANKI', 'F', '1990-01-27', '7878786754', 'PRIYANKA@GMAIL.COM', 'SAINIK COLONY', '2015-01-01', 002, '1111111111', 20000),
+    (DEFAULT, 'ANURAG', 'MEHTA', 'M', '1985-10-10', '9893657828', 'ANURAG77@GMAIL.COM', 'SECTOR 11', '2002-07-13', 003, '0001234567', 25000),
+    (DEFAULT, 'DHRUV', 'RATHEE', 'M', '1987-04-28', '9837489012', 'DHRUV@GMAIL.COM', 'SGM NAGAR', '2016-02-02', 001, '3336728190987', 30000),
+    (DEFAULT, 'RAHUL', 'RAHUL', 'M', '1995-04-17', '7834578298', 'RAHUL99@GMAIL.COM', 'BHOOR COLONY', '2019-08-19', 002, '012012012012', 21000),
+    (DEFAULT, 'SHYAM', 'LAL', 'M', '1980-12-09', '8666787692', 'SHYAM6767@GMAIL.COM)', 'KALYANPURI', '2000-03-01', 004, '5656267189012', 12000),
+    (DEFAULT, 'ANJALI', 'NAGAR', 'F', '1992-03-06', '87838982767', 'AJAYNN@GMAIL.COM', 'ANKIR', '2015-01-09',002, '901289035678', 18000),
+    (DEFAULT, 'VIJAY', 'AWASTHI', 'M', '1990-12-16', '9875626789', 'VIJAY77@GMAIL.COM', 'NIT 3', '2013-10-18', 02, '001235625428', 22000)
+;    
+
+
+-- INSERT INTO teacher TABLE
+INSERT INTO teacher
+VALUES
+    (1, 005),
+    (4, 004),
+    (6, 001),
+    (7, 002)
+;
+
+-- INSERT INTO classroom TABLE
+INSERT INTO classroom
+VALUES
+    (101, 100, 6),
+    (102, 101, 4),
+    (103, 102, 7),
+    (104, 103, 1),
+    (105, 001, NULL),
+    (106, 002, NULL),
+    (107, 003, NULL),
+    (108, 004, NULL),
+    (109, 005, NULL),
+    (110, 006, NULL),
+    (111, 007, NULL),
+    (112, 008, NULL),
+    (113, 009, NULL),
+    (114, 010, NULL),
+    (115, 011, NULL),
+    (116, 012, NULL)
+;
+
+--INSERT INTO classroom_student TABLE
+INSERT INTO classroom_student
+VALUES 
+    (101, 2),
+    (101, 3),
+    (101, 8),
+    (102, 1),
+    (102, 7),
+    (102, 5),
+    (103, 4),
+    (103, 6),
+    (104, 9),
+    (104, 11),
+    (104, 10)
+;
+
+--INSERT INTO attendance_student
+INSERT INTO attendance_student
+VALUES
+    (CURRENT_DATE, 1, TRUE),
+    (CURRENT_DATE, 2, TRUE),
+    (CURRENT_DATE, 3, FALSE),
+    (CURRENT_DATE, 4, TRUE),
+    (CURRENT_DATE, 5, TRUE),
+    (CURRENT_DATE, 6, FALSE),
+    (CURRENT_DATE, 7, TRUE),
+    (CURRENT_DATE, 8, TRUE),
+    (CURRENT_DATE, 9, FALSE),
+    (CURRENT_DATE, 10, TRUE),
+    (CURRENT_DATE, 11, TRUE)
+;
+
+--INSERT INTO attendance_employee
+INSERT INTO attendance_employee
+VALUES
+    (CURRENT_DATE, 1, TRUE),
+    (CURRENT_DATE, 2, TRUE),
+    (CURRENT_DATE, 3, FALSE),
+    (CURRENT_DATE, 4, TRUE),
+    (CURRENT_DATE, 5, TRUE),
+    (CURRENT_DATE, 6, FALSE)
+;
