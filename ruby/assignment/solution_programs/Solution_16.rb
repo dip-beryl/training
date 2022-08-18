39001c53e6e4f514fc0f791cda63e30cb65d68f3
dip-beryl <dipanshu.ranga@berylsystems.com>
2022-08-18 23:22:51 +0530

    ADD Solution_16

---
+++ ruby/assignment/solution_programs/Solution_16.rb
@@ -0,0 +1,33 @@
+=begin
+
+16. Given an integer limit being the upper limit of the range of interest, 
+    implement a function that 
+    returns the last 15 palindromes numbers lower or equal to limit as an array sorted ascendingly.
+    
+=end
+
+    
+def n_palindroms(num)
+    palindromes = []
+
+    1.upto(num) do |i|
+
+            # code to make reverse of a number -----------
+        reverse = 0
+        n = i
+
+        while (n > 0)     
+            reverse = (reverse * 10) + (n % 10)
+            n /= 10
+        end
+        #-------------------------------------------------
+
+        if (reverse == i)       #if reverse of the number is same then its a palindrom
+            palindromes << i
+        end
+    end
+    
+    return palindromes
+end
+
+print n_palindroms(150)
