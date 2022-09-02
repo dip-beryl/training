bebda1d54034e8bb67088a6342474e03c0155aa0
dip-beryl <dipanshu.ranga@berylsystems.com>
2022-09-02 12:07:43 +0530

    Add Validation Helper: comparison

--- rails/projects/lms/app/models/book.rb
+++ rails/projects/lms/app/models/book.rb
@@ -4,6 +4,8 @@ class Book < ApplicationRecord
     
     validates :terms_of_service, acceptance: {message: "Please accept t&c"}
 
+    validates :isbn, comparison: {greater_than: 100}
+
     # self.table_name = "book_details"
 	# self.primary_key = "isbn"
 end
