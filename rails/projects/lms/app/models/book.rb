45a57ca84b5cf7aaa8e5a88856fc1e695a88a015
dip-beryl <dipanshu.ranga@berylsystems.com>
2022-09-02 12:04:27 +0530

    Add Validation Helper: acceptance

--- rails/projects/lms/app/models/book.rb
+++ rails/projects/lms/app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
 
+    validates :name, presence: true
+    
+    validates :terms_of_service, acceptance: {message: "Please accept t&c"}
+
     # self.table_name = "book_details"
 	# self.primary_key = "isbn"
 end
