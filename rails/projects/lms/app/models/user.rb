e62deee75937a8d3859da4f38605866a41b5ba2f
dip-beryl <dipanshu.ranga@berylsystems.com>
2022-09-02 18:30:28 +0530

    Add Callbacks: before_validation, after_validation

--- rails/projects/lms/app/models/user.rb
+++ rails/projects/lms/app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
     validates :name, :email, presence: true
     
     before_validation :ensure_name_has_a_value
-    
+                
+    after_validation do
+        self.status = true unless(name.blank? and email.blank?)
+    end
+
     private
-        def ensure_name_has_a_value
-            if name.nil?
-                self.name = email unless email.blank?
-            end
+    def ensure_name_has_a_value
+        if name.nil?
+            self.name = email unless email.blank?
         end
+    end
+
 end
