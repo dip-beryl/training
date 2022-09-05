b54fac55487d6a2c3806f83e643c5300210e833e
dip-beryl <dipanshu.ranga@berylsystems.com>
2022-09-05 10:26:47 +0530

    Add Callback Method: around_save, _create, _update

--- rails/projects/lms/app/models/user.rb
+++ rails/projects/lms/app/models/user.rb
@@ -8,19 +8,33 @@ class User < ApplicationRecord
         self.status = true unless(name.blank? and email.blank?)
     end
 
+
     # before_save do
     #     self.name.upcase!
     # end
+    
 
-    after_save do
-        self.name.downcase!
-    end
+    # after_save do
+    #     self.name.downcase!
+    # end
+    
+
+    around_save :around_save_func
 
     private
-    def ensure_name_has_a_value
-        if name.nil?
-            self.name = email unless email.blank?
+        def ensure_name_has_a_value
+            if name.nil?
+                self.name = email unless email.blank?
+            end
         end
-    end
+
+        def around_save_func
+            self.name.upcase!       # all the code before [ yield ] is processed as before_save
+
+            yield
+
+            self.name.downcase!      # all the code after [ yield ] is processed as after_save
+        end
+
 
 end
