c3aa4a06f04ae73d7582cd12b9fbb01b094cf2c4
dip-beryl <dipanshu.ranga@berylsystems.com>
2022-09-08 19:10:05 +0530

    Add ActiveRecord_Query: scope

--- rails/projects/lms/app/models/order.rb
+++ rails/projects/lms/app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
     belongs_to :customer
 
     enum :status, [:shipped, :processing, :cancelled, :delivered]
+
+    scope :in_process, -> { where(status: 1)}
+
+    scope :in_process_and_very_old, -> { in_process.where("created_at > '2022-07-01'")}
+
+    scope :costs_more_than, ->(amount) { where("total > ?", amount) }
+
+
 end
